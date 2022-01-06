3dc2ed570dac42f0296ea6e97b877ec143db25c7
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-01-06 15:45:03 +0900

    changes home btn colors to different shades

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,34 +1,32 @@
-import { FunctionComponent } from 'react';
-import HomeNavItem from './HomeNavitem';
+import { FunctionComponent } from "react";
+import HomeNavItem from "./HomeNavitem";
 
-interface HomeProps {
-    
-}
- 
+interface HomeProps {}
+
 const Home: FunctionComponent<HomeProps> = () => {
-    return (
-        <div className="flex justify-center">
-            <nav className="pt-12">
-                <div className="bg-gradient-to-r from-blue-100 to-cyan-100 rounded drop-shadow">
-                    <HomeNavItem 
-                        title="Tokens" 
-                        path="/tokens" 
-                        message="Launch your project tokens and manage them"
-                    />
-                </div>
-                <div className="bg-gradient-to-r from-cyan-100 to-blue-100 rounded drop-shadow">
-                    <HomeNavItem 
-                        title="Growth" 
-                        path="/growth" 
-                        message="Grow your project and make friends"
-                    />
-                </div>
-            </nav>
+  return (
+    <div className="flex justify-center">
+      <nav className="pt-12">
+        <div className="bg-gradient-to-r from-blue-100 to-cyan-100 rounded drop-shadow">
+          <HomeNavItem
+            title="Tokens"
+            path="/tokens"
+            message="Launch your project tokens and manage them"
+          />
         </div>
-    );
-}
+        <div className="bg-gradient-to-r from-green-100 to-yellow-100 rounded drop-shadow">
+          <HomeNavItem
+            title="Growth"
+            path="/growth"
+            message="Grow your project and make friends"
+          />
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 // 243, 170, 60
 // 250, 250, 220
- 
+
 export default Home;
