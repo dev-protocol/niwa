e610e3d37781277ba10540cf3b42352fc74dfc4d
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-01-14 15:37:15 +0900

    tokenize invitation page removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import TokensPage from './pages/tokens'
 import GrowthPage from './pages/growth'
 import ConnectButton from './components/ConnectButton'
 import { useWeb3ProviderContext, WebProviderContext } from './context/web3ProviderContext'
 import TokenizeMarketSelect from './pages/tokenize-market-select'
-import InvitationRequestPage from './pages/tokenize-invitation'
 
 function App() {
   const web3ProviderContext = useWeb3ProviderContext()
@@ -31,11 +30,6 @@ function App() {
               <Route path="/tokens" element={<TokensPage />} />
               <Route path="/growth" element={<GrowthPage />} />
               <Route path="/tokenize" element={<TokenizeMarketSelect />} />
-              <Route path="/tokenize/:market/invitation-request" element={<InvitationRequestPage />} />
-              <Route
-                path="/tokenize/:market"
-                element={<Navigate replace to="/tokenize/:market/invitation-request" />}
-              />
             </Routes>
           </BrowserRouter>
         </main>
