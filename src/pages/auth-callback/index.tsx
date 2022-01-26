5a0efd1759cda2cdc3fb87d5167a07073fe454bf
Hideo Hattori <hhatto.jp@gmail.com>
2022-01-26 22:32:44 +0900

    wip: add to google oauth callback page

--- src/pages/auth-callback/index.tsx
+++ src/pages/auth-callback/index.tsx
@@ -1,15 +1,80 @@
 import { FunctionComponent, useEffect, useState } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate, useLocation } from 'react-router-dom'
+import useSWR from 'swr'
 import { Market } from '../../const'
 import { getMarketFromString } from '../../utils/utils'
 import { UndefinedOr } from '@devprotocol/util-ts'
 
 interface AuthCallbackPageProps {}
 
+interface QueryString {
+  [key: string]: string
+}
+
 const AuthCallbackPage: FunctionComponent<AuthCallbackPageProps> = () => {
   const params = useParams()
+  const { search, hash } = useLocation()
+  const target = search ? search : hash
+  const queryParams = target
+    .slice(1)
+    .split('&')
+    .map((str) => [str.split('=')[0], str.split('=')[1]])
+    .reduce((acc, a) => {
+      acc[a[0]] = a[1];
+      return acc;
+    }, {} as QueryString)
   const navigate = useNavigate()
+  const [isVerify, setIsVerify] = useState(false)
   const [market, setMarket] = useState<UndefinedOr<Market>>()
+
+  const clientId = import.meta.env.VITE_YOUTUBE_CLIENT_ID
+  const swrOptions = {
+    revalidateOnFocus: false,
+    shouldRetryOnError: false,
+    focusThrottleInterval: 0,
+  }
+
+  const accessToken = queryParams.access_token || ''
+  const { data: verifyData } = useSWR(
+    accessToken !== '' ? 'google/verify' : null,
+    async () => {
+      const verifyUrl = `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+      return fetch(verifyUrl).then((res: any) => {
+        return res.json().then((value: any) => {
+          const email = value.email
+          if (value.audience !== clientId) {
+            throw new Error('invalid audience')
+          }
+          setIsVerify(true)
+          return { email }
+        })
+      })
+    },
+    swrOptions
+  )
+  const { data: youtubeData } = useSWR(
+    accessToken !== '' ? 'youtube/dataapi' : null,
+    async () => {
+      const youtubeDataApiUrl = `https://www.googleapis.com/youtube/v3/channels?part=id,snippet,brandingSettings,statistics&mine=true&access_token=${accessToken}`
+      return fetch(youtubeDataApiUrl).then((res: any) => {
+        const data = res.json()
+        return data.then((value: any) => {
+          return value.items.map((item: any) => {
+            const channelId = item.id
+            const videoCount = item.statistics.videoCount
+            const viewCount = item.statistics.viewCount
+            const keywords = item.brandingSettings.channel.keywords
+            const title = item.snippet.title
+            const description = item.snippet.description
+            return { channelId, videoCount, viewCount, keywords, title, description }
+          })
+        })
+      })
+    },
+    swrOptions
+  )
+
+  console.log('verify:', verifyData, youtubeData, accessToken, params, queryParams)
 
   useEffect(() => {
     const _market = getMarketFromString(params.market)
@@ -21,7 +86,11 @@ const AuthCallbackPage: FunctionComponent<AuthCallbackPageProps> = () => {
       return
     }
 
-    // TODO: verify access_token
+    if (!accessToken || !verifyData || !youtubeData || !isVerify) {
+      return
+    }
+
+    // TODO: store data for tokenize
   }, [params, navigate, setMarket, market])
 
   return (
