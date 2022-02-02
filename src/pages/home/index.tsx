3ed4aa7df107522912d7dccd94cf8422e4a221b3
CryptoDogg <d.enkikairo@gmail.com>
2022-02-02 11:08:56 +0900

    wip s-token

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,30 +1,16 @@
 import React from 'react'
 import HomeNavItem from './HomeNavitem'
 import STokenPositionDetail from '../supporters'
+import Token from '../s-token'
 
 interface HomeProps { }
 
 const Home: React.FC<HomeProps> = () => {
-<<<<<<< HEAD
-  const { ethersProvider } = useProvider()
-  const [userTokensPath, setUserTokensPath] = useState(EMPTY_USER_TOKEN_PATH)
-  useEffect(() => {
-    if (!ethersProvider) {
-      setUserTokensPath(EMPTY_USER_TOKEN_PATH)
-      return
-    }
-    ; (async () => {
-      const address = await ethersProvider.getSigner().getAddress()
-      setUserTokensPath(address ?? EMPTY_USER_TOKEN_PATH)
-    })()
-  }, [ethersProvider])
-
-=======
->>>>>>> 5e1ab509468ac48846ce6e660d3340146e5735fb
   return (
     <section className="grid-2">
       <HomeNavItem title="Tokens" path="/tokenize" message="Launch your project tokens and manage them" />
       <HomeNavItem title="Growth" path="/growth" message="Grow your project and make friends" />
+      <Token />
     </section>
   )
 }
