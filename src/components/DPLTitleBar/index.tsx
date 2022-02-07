0f4dd18b9979215a07a09da7377a76d459ea5527
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-02-07 23:52:46 +0900

    gradients theming updated

--- src/components/DPLTitleBar/index.tsx
+++ src/components/DPLTitleBar/index.tsx
@@ -8,7 +8,7 @@ interface DPLTitleBarProps {
 const DPLTitleBar: React.FC<DPLTitleBarProps> = ({ title, sub }) => {
   return (
     <>
-      <h1 className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-cyan-600 mb-8">
+      <h1 className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-primary to-secondary mb-8">
         {title}
       </h1>
       {sub && <h2 className="mb-4 text-xl">{sub}</h2>}
