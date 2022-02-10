c2ae79c850ca89a90f51a4100663ccd4cd049080
aggre <hiroyuki.aggre@gmail.com>
2022-02-10 10:14:55 +0900

    add tweet link

---
+++ src/components/Tweet/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaTwitter } from 'react-icons/fa'
+
+interface TweetProps {
+  params: { [key: string]: string }
+  className?: string
+}
+
+const TweetLarge: React.FC<TweetProps> = ({ params, className, children }) => {
+  return (
+    <a
+      href={`https://twitter.com/intent/tweet?${new URLSearchParams(params).toString()}`}
+      target="_blank"
+      rel="norefferer  noopener noreferrer"
+      className={`inline-grid gap-1 justify-items-center ${className}`}
+    >
+      <span>{children}</span>
+      <FaTwitter size="3em" />
+    </a>
+  )
+}
+
+export { TweetLarge }
