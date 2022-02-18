f13a9beba8bd1ef75b1ccf29b48f2e0be75e8f62
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-02-18 16:06:34 +0900

    joins properties into nested routes; shares propertyDetails data

---
+++ src/pages/properties/index.tsx
@@ -0,0 +1,43 @@
+import { UndefinedOr } from '@devprotocol/util-ts'
+import React from 'react'
+import { Outlet, useOutletContext, useParams } from 'react-router-dom'
+import Card from '../../components/Card'
+import PropertySummaryHead from './ProperySummary'
+import { SectionLoading } from '../../components/Spinner'
+import { PropertyDetails, usePropertyDetails } from '../../hooks/usePropertyDetails'
+
+interface PropertyOutletProps {}
+
+type ContextType = {
+  propertyDetails: PropertyDetails | null
+  isPropertyDetailsLoading: boolean
+  propertyDetailsError: UndefinedOr<String>
+}
+
+export function usePropertyOutlet() {
+  return useOutletContext<ContextType>()
+}
+
+const PropertyOutlet: React.FC<PropertyOutletProps> = () => {
+  const { hash } = useParams()
+  const { propertyDetails, isLoading, error } = usePropertyDetails(hash)
+
+  return (
+    <div>
+      {hash && propertyDetails && !isLoading && (
+        <>
+          <PropertySummaryHead propertyDetails={propertyDetails} hash={hash} isActive="details" />
+          <Outlet context={{ propertyDetails, isPropertyDetailsLoading: isLoading, propertyDetailsError: error }} />
+        </>
+      )}
+      {isLoading && <SectionLoading />}
+      {error && (
+        <Card>
+          <div className="text-red-500">{error}</div>
+        </Card>
+      )}
+    </div>
+  )
+}
+
+export default PropertyOutlet
