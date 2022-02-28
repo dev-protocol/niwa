57a2d5c0eb2b0d2fa0cbcfc251e023cc1561f519
aggre <hiroyuki.aggre@gmail.com>
2022-02-28 12:27:24 +0900

    Add information to the root page

--- src/pages/network-select/index.tsx
+++ src/pages/network-select/index.tsx
@@ -5,32 +5,38 @@ interface NetworkSelectPageProps {}
 
 const NetworkSelectPage: React.FC<NetworkSelectPageProps> = () => {
   return (
-    <section className="grid grid-rows-2">
-      <HomeNavItem
-        title="Arbitrum"
-        path="https://arbitrum.niwa-beta.devprotocol.xyz/"
-        message="Stake on your favorite projects using the Arbitrum Network"
-        isExternal={true}
-      />
-      <HomeNavItem
-        title="Polygon"
-        path="https://polygon.niwa-beta.devprotocol.xyz/"
-        message="Stake on your favorite projects using the Polygon Network"
-        isExternal={true}
-      />
-      <HomeNavItem
-        title="Arbitrum Testnet"
-        path="https://arbitrum-rinkeby.niwa-beta.devprotocol.xyz/"
-        message="Explore the Dev Protocol on the Arbitrum Testnet"
-        isExternal={true}
-      />
-      <HomeNavItem
-        title="Polygon Testnet"
-        path="https://polygon-mumbai.niwa-beta.devprotocol.xyz/"
-        message="Explore the Dev Protocol on the Polygon Mumbai Testnet"
-        isExternal={true}
-      />
-    </section>
+    <article className="grid gap-6">
+      <section>
+        <img src="/src/img/og.png" className="rounded-lg shadow"></img>
+      </section>
+      <section className="grid gap-4">
+        <h2 className="text-6xl font-bold">Choose network</h2>
+        <HomeNavItem
+          title="Arbitrum"
+          path="https://arbitrum.niwa.xyz/"
+          message="Stake on your favorite projects using the Arbitrum Network"
+          isExternal={true}
+        />
+        <HomeNavItem
+          title="Polygon"
+          path="https://polygon.niwa.xyz/"
+          message="Stake on your favorite projects using the Polygon Network"
+          isExternal={true}
+        />
+        <HomeNavItem
+          title="Arbitrum Testnet"
+          path="https://arbitrum-rinkeby.niwa.xyz/"
+          message="Explore the Dev Protocol on the Arbitrum Testnet"
+          isExternal={true}
+        />
+        <HomeNavItem
+          title="Polygon Testnet"
+          path="https://polygon-mumbai.niwa.xyz/"
+          message="Explore the Dev Protocol on the Polygon Mumbai Testnet"
+          isExternal={true}
+        />
+      </section>
+    </article>
   )
 }
 
