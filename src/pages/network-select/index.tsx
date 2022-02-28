4c8c59259a2c6ef3100417a652b66aea18eebc2b
aggre <hiroyuki.aggre@gmail.com>
2022-02-28 13:29:18 +0900

    add "how it works" page and tweaks some texts

--- src/pages/network-select/index.tsx
+++ src/pages/network-select/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import HomeNavItem from '../../components/HomeNavItem'
+import HSButton from '../../components/HSButton'
 import Eyecatching from '../../img/og.png'
 
 interface NetworkSelectPageProps {}
@@ -7,33 +9,38 @@ interface NetworkSelectPageProps {}
 const NetworkSelectPage: React.FC<NetworkSelectPageProps> = () => {
   return (
     <article className="grid gap-6">
-      <section>
+      <section className="grid gap-4">
         <img src={Eyecatching} className="rounded-lg shadow"></img>
+        <p>
+          <HSButton link="/how-it-works" type="filled">
+            Connect Wallet
+          </HSButton>
+        </p>
       </section>
       <section className="grid gap-4">
         <h2 className="text-6xl font-bold">Choose network</h2>
         <HomeNavItem
           title="Arbitrum"
           path="https://arbitrum.niwa.xyz/"
-          message="Stake on your favorite projects using the Arbitrum Network"
+          message="Tokenize your project on Arbitrum"
           isExternal={true}
         />
         <HomeNavItem
           title="Polygon"
           path="https://polygon.niwa.xyz/"
-          message="Stake on your favorite projects using the Polygon Network"
+          message="Tokenize your project on Polygon"
           isExternal={true}
         />
         <HomeNavItem
           title="Arbitrum Testnet"
           path="https://arbitrum-rinkeby.niwa.xyz/"
-          message="Explore the Dev Protocol on the Arbitrum Testnet"
+          message="Explore tokenizing your project on the Arbitrum Testnet"
           isExternal={true}
         />
         <HomeNavItem
           title="Polygon Testnet"
           path="https://polygon-mumbai.niwa.xyz/"
-          message="Explore the Dev Protocol on the Polygon Mumbai Testnet"
+          message="Explore tokenizing your project on the Polygon Testnet"
           isExternal={true}
         />
       </section>
